af3fdeb8a718c974d274d7dc6949bebc76600257
vk13194 <vijaym13194@gmail.com>
2020-12-08 14:09:39 +0530

    remove commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
-
+require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 8080; // Step 1
 
